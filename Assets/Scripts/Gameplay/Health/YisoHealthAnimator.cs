5099833d0fcd0b78093440280f0b2985c942c83a
beagle <chldusdn20@gmail.com>
2025-08-28 16:31:40 +0900

    feat: add health animator component

--- Assets/Scripts/Gameplay/Health/YisoHealthAnimator.cs
+++ Assets/Scripts/Gameplay/Health/YisoHealthAnimator.cs
@@ -1,17 +1,104 @@
-﻿using Core.Behaviour;
+﻿using System;
+using System.Collections.Generic;
+using Core.Behaviour;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace Gameplay.Health {
+    [Serializable]
+    public struct AnimatorParameterAction {
+        public AnimatorControllerParameterType parameterType;
+        public string parameterName;
+        public float floatValue;
+        public bool boolValue;
+        public int intValue;
+        [HideInInspector] public int parameterHash;
+    }
+
     [AddComponentMenu("Yiso/Health/Health Animator")]
-    public class YisoHealthAnimator: RunIBehaviour {
-        [SerializeField] private bool updateAnimatorParameters = false;
-        [ShowIf("updateAnimatorParameters")] public string damagedAnimationParameterName;
-        [ShowIf("updateAnimatorParameters")] public string deathAnimationParameterName;
-        
+    public class YisoHealthAnimator : RunIBehaviour {
+        [Title("Animation Settings")] [Tooltip("체크 해제 시 이 컴포넌트의 모든 애니메이션 업데이트 기능이 비활성화됩니다.")] [SerializeField]
+        private bool updateAnimatorParameters = true;
+
+        [Tooltip("데미지를 받았을 때 실행할 애니메이션 파라미터 목록입니다.")] [SerializeField]
+        private List<AnimatorParameterAction> onDamageActions;
+
+        [Tooltip("사망했을 때 실행할 애니메이션 파라미터 목록입니다.")] [SerializeField]
+        private List<AnimatorParameterAction> onDeathActions;
+
+        [Tooltip("부활했을 때 실행할 애니메이션 파라미터 목록입니다.")] [SerializeField]
+        private List<AnimatorParameterAction> onReviveActions;
+
+        private YisoEntityHealth _entityHealth;
         private Animator _animator;
-        
-        // TODO: 리스너 (Listener) 이벤트를 구독하여 애니메이션 재생
-        // Bool, Trigger, Int, Float 모두 받기 -> struct 각각 만들면 되겠네.
+
+        protected override void Awake() {
+            base.Awake();
+            _animator = GetComponentInChildren<Animator>();
+            _entityHealth = GetComponent<YisoEntityHealth>();
+
+            InitializeParameterHashes();
+        }
+
+        protected override void OnEnable() {
+            base.OnEnable();
+            if (_entityHealth == null) return;
+            _entityHealth.OnDamaged += HandleDamage;
+            _entityHealth.OnDied += HandleDeath;
+            _entityHealth.OnRevived += HandleRevive;
+        }
+
+        protected override void OnDisable() {
+            base.OnDisable();
+            if (_entityHealth == null) return;
+            _entityHealth.OnDamaged -= HandleDamage;
+            _entityHealth.OnDied -= HandleDeath;
+            _entityHealth.OnRevived -= HandleRevive;
+        }
+
+        private void HandleDamage(DamageInfo damageInfo) => ApplyActions(onDamageActions);
+        private void HandleDeath() => ApplyActions(onDeathActions);
+        private void HandleRevive() => ApplyActions(onReviveActions);
+
+        private void ApplyActions(List<AnimatorParameterAction> actions) {
+            if (!updateAnimatorParameters || _animator == null || actions == null) {
+                return;
+            }
+
+            foreach (var action in actions) {
+                switch (action.parameterType) {
+                    case AnimatorControllerParameterType.Trigger:
+                        _animator.SetTrigger(action.parameterHash);
+                        break;
+                    case AnimatorControllerParameterType.Bool:
+                        _animator.SetBool(action.parameterHash, action.boolValue);
+                        break;
+                    case AnimatorControllerParameterType.Int:
+                        _animator.SetInteger(action.parameterHash, action.intValue);
+                        break;
+                    case AnimatorControllerParameterType.Float:
+                        _animator.SetFloat(action.parameterHash, action.floatValue);
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+
+        private void InitializeParameterHashes() {
+            ConvertListToHashes(onDamageActions);
+            ConvertListToHashes(onDeathActions);
+            ConvertListToHashes(onReviveActions);
+        }
+
+        private static void ConvertListToHashes(IList<AnimatorParameterAction> actions) {
+            if (actions == null) return;
+            for (var i = 0; i < actions.Count; i++) {
+                var action = actions[i];
+                if (string.IsNullOrEmpty(action.parameterName)) continue;
+                action.parameterHash = Animator.StringToHash(action.parameterName);
+                actions[i] = action;
+            }
+        }
     }
 }
