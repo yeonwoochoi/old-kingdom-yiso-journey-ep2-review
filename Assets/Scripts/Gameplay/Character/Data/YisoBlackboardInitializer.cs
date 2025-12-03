edfb9a505081e67778903c90c304c96a50963cfc
beagle <chldusdn20@gmail.com>
2025-12-03 13:23:17 +0900

    feat: add support for current position in Blackboard vector initialization
    - Add `useCurrentPosition` flag to `VectorEntry` in `YisoBlackboardInitializer`.
    - Allow automatic assignment of `transform.position` instead of manual Vector3 input.
    - Utilize Odin attributes to hide manual input field when the flag is active.
    - Improve scalability for dynamic location data such as SpawnPosition.

--- Assets/Scripts/Gameplay/Character/Data/YisoBlackboardInitializer.cs
+++ Assets/Scripts/Gameplay/Character/Data/YisoBlackboardInitializer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Core.Behaviour;
 using Gameplay.Character.Core;
 using Gameplay.Character.Core.Modules;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace Gameplay.Character.Data {
@@ -11,8 +12,20 @@ namespace Gameplay.Character.Data {
         [Serializable] public class IntEntry { public YisoBlackboardKeySO key; public int value; }
         [Serializable] public class StringEntry { public YisoBlackboardKeySO key; public string value; }
         [Serializable] public class BoolEntry { public YisoBlackboardKeySO key; public bool value; }
-        [Serializable] public class VectorEntry { public YisoBlackboardKeySO key; public Vector3 value; }
         [Serializable] public class UnityObjectEntry { public YisoBlackboardKeySO key; public UnityEngine.Object value; }
+
+        [Serializable]
+        public class VectorEntry {
+            [HorizontalGroup("Line", 0.4f), HideLabel]
+            public YisoBlackboardKeySO key;
+            
+            [HorizontalGroup("Line", 0.2f), LabelText("Current Pos")]
+            public bool useCurrentPosition;
+            
+            [HorizontalGroup("Line"), HideLabel]
+            [HideIf(nameof(useCurrentPosition))] // 체크되면 수동 입력 필드 숨김
+            public Vector3 value;
+        }
 
         [SerializeField] private List<FloatEntry> _floatEntries = new();
         [SerializeField] private List<IntEntry> _intEntries = new();
@@ -41,8 +54,11 @@ namespace Gameplay.Character.Data {
             foreach (var e in _intEntries) module.SetInt(e.key, e.value);
             foreach (var e in _stringEntries) module.SetString(e.key, e.value);
             foreach (var e in _boolEntries) module.SetBool(e.key, e.value);
-            foreach (var e in _vectorEntries) module.SetVector(e.key, e.value);
             foreach (var e in _objectEntries) module.SetObject(e.key, e.value);
+            foreach (var e in _vectorEntries) {
+                var finalValue = e.useCurrentPosition ? context.Transform.position : e.value;
+                module.SetVector(e.key, finalValue);
+            }
         }
     }
 }
