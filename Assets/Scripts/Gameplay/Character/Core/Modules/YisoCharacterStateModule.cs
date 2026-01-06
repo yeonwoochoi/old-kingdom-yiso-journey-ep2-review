51b11c854e87847e5b70e33080865dd66983a02c
beagle <chldusdn20@gmail.com>
2026-01-06 23:34:48 +0900

    FSM 테스트 씬 초기화 및 샘플 데이터 추가

--- Assets/Scripts/Gameplay/Character/Core/Modules/YisoCharacterStateModule.cs
+++ Assets/Scripts/Gameplay/Character/Core/Modules/YisoCharacterStateModule.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Gameplay.Character.StateMachine;
+using Gameplay.Character.Types;
 using UnityEngine;
 
 namespace Gameplay.Character.Core.Modules {
@@ -13,20 +14,22 @@ namespace Gameplay.Character.Core.Modules {
 
         public override void Initialize() {
             base.Initialize();
-            if (_settings.stateMachine != null) {
-                _stateMachine = _settings.stateMachine;
-            }
+            if (Context.Type != CharacterType.Player) {
+                if (_settings.stateMachine != null) {
+                    _stateMachine = _settings.stateMachine;
+                }
             
-            if (_stateMachine == null) {
-                Debug.LogError($"[YisoCharacterStateModule] StateMachine을 찾을 수 없습니다! {Context.GameObject.name}");
-            }
+                if (_stateMachine == null) {
+                    Debug.LogError($"[YisoCharacterStateModule] StateMachine을 찾을 수 없습니다! {Context.GameObject.name}");
+                }
             
-            _settings.stateMachine.PreInitialize(Context);
+                _settings.stateMachine.PreInitialize(Context);
+            }
         }
 
         public override void LateInitialize() {
             base.LateInitialize();
-            _settings.stateMachine.Initialize();
+            _stateMachine?.Initialize();
         }
 
         [Serializable]
