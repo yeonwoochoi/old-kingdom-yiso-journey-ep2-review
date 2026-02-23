9f5aa724f931ed39ab58654559e6ca30436f8634
beagle <chldusdn20@gmail.com>
2026-02-23 19:55:34 +0900

    디코딩 에러나서 주석 제거

--- Client/Assets/Scripts/Utils/YisoLogger.cs
+++ Client/Assets/Scripts/Utils/YisoLogger.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Runtime.CompilerServices; // 이것이 필요합니다.
+using System.Runtime.CompilerServices; // 占싱곤옙占쏙옙 占십울옙占쌌니댐옙.
 using UnityEngine;
 using Debug = UnityEngine.Debug;
 
@@ -10,11 +10,8 @@ namespace Utils
     public static class YisoLogger
     {
         private const string DEFINE_SYMBOL = "ENABLE_LOG";
-        private const string COLOR_INFO = "white"; // 기본 색상
-
-        // 로그 포맷: [스크립트이름::메서드이름] 메시지
-        // 예: [PlayerController::Move] 이동 시작
-
+        private const string COLOR_INFO = "white";
+        
         [Conditional(DEFINE_SYMBOL)]
         public static void Log(
             object message,
@@ -33,7 +30,6 @@ namespace Utils
             [CallerMemberName] string memberName = "")
         {
             string scriptName = GetScriptName(filePath);
-            // context가 있으면 유니티 콘솔에서 클릭 시 해당 오브젝트 하이라이팅됨
             Debug.Log($"<color={COLOR_INFO}>[{scriptName}::{memberName}]</color> {message}", context);
         }
 
@@ -50,12 +46,11 @@ namespace Utils
         [Conditional(DEFINE_SYMBOL)]
         public static void LogWarning(
             object message,
-            Object context, // context 추가
+            Object context,
             [CallerFilePath] string filePath = "",
             [CallerMemberName] string memberName = "")
         {
             string scriptName = GetScriptName(filePath);
-            // Debug.LogWarning에도 context 전달
             Debug.LogWarning($"[{scriptName}::{memberName}] {message}", context);
         }
 
@@ -72,32 +67,24 @@ namespace Utils
         [Conditional(DEFINE_SYMBOL)]
         public static void LogError(
             object message,
-            Object context, // context 추가
+            Object context,
             [CallerFilePath] string filePath = "",
             [CallerMemberName] string memberName = "")
         {
             string scriptName = GetScriptName(filePath);
-            // Debug.LogError에도 context 전달
             Debug.LogError($"[{scriptName}::{memberName}] {message}", context);
         }
 
         private static readonly Dictionary<string, string> _fileNameCache = new Dictionary<string, string>();
 
-        // 파일 경로에서 스크립트 이름만 추출하는 헬퍼
         private static string GetScriptName(string filePath)
         {
-            // 캐싱된거 있는지 확인
             if (_fileNameCache.TryGetValue(filePath, out string scriptName))
             {
                 return scriptName;
             }
-
-            // 없으면 계산 (문자열 연산 발생)
             scriptName = Path.GetFileNameWithoutExtension(filePath);
-
-            // 계산 결과 캐싱
             _fileNameCache[filePath] = scriptName;
-
             return scriptName;
         }
     }
