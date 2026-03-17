fe0459f874a81e7316c64b6b37eb376ab48cd753
beagle <chldusdn20@gmail.com>
2026-03-18 02:09:50 +0900

    Scene 시스템 추가 (YisoSceneSystem, SceneEvent - 씬 비동기 로딩/전환)

---
+++ Client/Assets/Scripts/Core/Scene/SceneEvent.cs
@@ -0,0 +1,35 @@
+﻿using Core.Event;
+
+namespace Core.Scene {
+    public enum YisoSceneType {
+        Login,
+        BaseCamp,
+        Chapter,
+        InfiniteDojo,
+        Transition
+    }
+    
+    public struct SceneTransitionStartEvent {
+        public YisoSceneType From;
+        public YisoSceneType To;
+
+        public static void TriggerEvent(YisoSceneType from, YisoSceneType to) {
+            YisoEventSystem.TriggerEvent(new SceneTransitionStartEvent {
+                From = from,
+                To = to
+            });
+        }
+    }
+    
+    public struct SceneTransitionCompleteEvent {
+        public YisoSceneType From;
+        public YisoSceneType To;
+
+        public static void TriggerEvent(YisoSceneType from, YisoSceneType to) {
+            YisoEventSystem.TriggerEvent(new SceneTransitionCompleteEvent {
+                From = from,
+                To = to
+            });
+        }
+    }
+}
