2817615c80258045c464a6fbd6ad0b14ae30aee4
beagle <chldusdn20@gmail.com>
2026-03-18 02:09:51 +0900

    Sound 시스템 추가 (YisoSoundSystem, YisoSoundDataSO - BGM/SFX Addressable 관리)

---
+++ Client/Assets/Scripts/Core/Sound/YisoSoundDataSO.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace Core.Sound {
+    public enum YisoSoundId {
+        // BGM
+        BgmLogin,
+        BgmBaseCamp1,
+        BgmBaseCamp2,
+        
+        // SFX
+        SfxAttack,
+        SfxHit,
+    }
+    
+    [CreateAssetMenu(fileName = "SoundSO", menuName = "Yiso/Config/Sound")]
+    public class YisoSoundDataSO: ScriptableObject {
+        [Serializable]
+        public class Entry {
+            public YisoSoundId id;
+            public AssetReferenceT<AudioClip> clipRef;
+        }
+        
+        [SerializeField] private List<Entry> entries;
+        private Dictionary<YisoSoundId, AssetReferenceT<AudioClip>> _map;
+            
+        public void Build() {
+            _map = entries.ToDictionary(e => e.id, e => e.clipRef);
+        }
+
+        public AssetReferenceT<AudioClip> Get(YisoSoundId id) => _map[id];
+    }
+}
