bad2b65472640d19c355d6406ba74875ebbe23d3
beagle <chldusdn20@gmail.com>
2026-03-18 02:10:04 +0900

    Platform 시스템 추가 (YisoPlatformSystem)

---
+++ Client/Assets/Scripts/Core/Platform/YisoPlatformSystem.cs
@@ -0,0 +1,11 @@
+﻿using Core.Singleton;
+
+namespace Core.Platform {
+    public class YisoPlatformSystem: YisoSingleton<YisoPlatformSystem> {
+#if UNITY_EDITOR && (UNITY_ANDROID || UNITY_IOS)
+        public bool IsMobile => true;
+#else
+        public bool IsMobile => false;
+#endif
+    }
+}
